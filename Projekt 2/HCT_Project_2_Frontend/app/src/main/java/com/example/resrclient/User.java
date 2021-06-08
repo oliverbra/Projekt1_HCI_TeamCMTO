c3462906038aead4d8a5352f9a72a1b58a5d4ca2
Clara Kuntz <44357886+claraku@users.noreply.github.com>
2021-06-08 21:19:36 +0200

    Add User class

---
+++ Projekt 2/HCT_Project_2_Frontend/app/src/main/java/com/example/resrclient/User.java
@@ -0,0 +1,34 @@
+package com.example.resrclient;
+
+public class User {
+
+    private String id;
+    private String name;
+    //private Progress progress;
+    //private ArrayList<GrowSpace> ownGS;
+    //private ArrayList<GrowSpace> favouriteGS;
+    //private ArrayList<Review> sentReviews;
+
+
+    public User(String pId, String pName){
+            id = pId;
+            name = pName;
+    }
+
+    //add GD to ownGS
+    //public GrowSpace createGS(){}
+
+    //delete GS from ownGS
+    //public void deleteGS(){}
+
+    //edit a GS in ownGS
+    //public void editGS(){}
+
+    //favor a GS
+    //public void favorGS(){}
+
+    //review a GS
+    //public Review reviewGS(){}
+
+    //generate getter and setter automatically?
+}
