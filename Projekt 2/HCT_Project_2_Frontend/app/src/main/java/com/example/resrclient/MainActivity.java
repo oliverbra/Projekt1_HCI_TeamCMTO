50bda6b2a9fec7d81250049306a9f6f48b177764
Oli <44357868+oliverbra@users.noreply.github.com>
2021-06-20 10:00:28 +0200

    Update MainActivity.java

--- Projekt 2/HCT_Project_2_Frontend/app/src/main/java/com/example/resrclient/MainActivity.java
+++ Projekt 2/HCT_Project_2_Frontend/app/src/main/java/com/example/resrclient/MainActivity.java
@@ -84,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
         // Ende
 
 
-        // OBA  192.168.0.135 - url set to local host; not vm
+        // OBA  192.168.0.135 - (Oli: url set to local host; not vm)
         public void sendMessage(View view) {
             final String url = "http://10.0.2.2:8080/personAli";
             new RESTTask().execute(url);
