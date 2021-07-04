768d6b2b48cfed11cff64a63171408299a27d4ab
kochtill <43991025+kochtill@users.noreply.github.com>
2021-07-04 17:18:26 +0200

    Verlinkung zwischen den Screens

--- Projekt 2/HCT_Project_2_Frontend/app/src/main/java/com/example/resrclient/activity_startseite.java
+++ Projekt 2/HCT_Project_2_Frontend/app/src/main/java/com/example/resrclient/activity_startseite.java
@@ -2,7 +2,9 @@ package com.example.resrclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class activity_startseite extends AppCompatActivity {
 
@@ -11,4 +13,12 @@ public class activity_startseite extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startseite);
     }
+
+    public void chanceProgress(View view) {
+        Intent intent = new Intent(this, activity_progress.class);
+        startActivity(intent); }
+
+    public void chanceGS(View view) {
+        Intent intent = new Intent(this, activity_growspace.class);
+        startActivity(intent); }
 }
