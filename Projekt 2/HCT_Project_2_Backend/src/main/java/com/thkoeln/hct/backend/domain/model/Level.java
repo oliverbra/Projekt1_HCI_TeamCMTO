66ccb1d9ce93c9932a67d5cdf905c5fbf27ebd9e
aobaidi410 <81808059+aobaidi410@users.noreply.github.com>
2021-07-22 12:37:56 +0200

    New Tabel Level

---
+++ Projekt 2/HCT_Project_2_Backend/src/main/java/com/thkoeln/hct/backend/domain/model/Level.java
@@ -0,0 +1,32 @@
+package com.thkoeln.hct.backend.domain.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "level")
+
+
+
+public class Level {
+
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(nullable = false)
+    private Integer levelId;
+    private Integer level;
+    private String progressTier;
+    private String progressTierequirement;
+    private String comment;
+    private String reviews;
+   // @ManyToOne(cascade  = CascadeType.ALL)
+    //List<User> users;
+}
+
