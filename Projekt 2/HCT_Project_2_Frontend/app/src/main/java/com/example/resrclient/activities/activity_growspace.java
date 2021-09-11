8b4c0fb046199e00c0291b3076213c54490ca149
kochtill <43991025+kochtill@users.noreply.github.com>
2021-09-11 16:31:56 +0200

    Navigation Bar added
    Navigations Bar für LogiN, Home und GrowSpace als Beispiele Hinzugefügt

--- Projekt 2/HCT_Project_2_Frontend/app/src/main/java/com/example/resrclient/activities/activity_growspace.java
+++ Projekt 2/HCT_Project_2_Frontend/app/src/main/java/com/example/resrclient/activities/activity_growspace.java
@@ -1,19 +1,50 @@
 package com.example.resrclient.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.resrclient.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class activity_growspace extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView; //Navigation
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_growspace);
+
+
+        //Navigation
+        bottomNavigationView=findViewById(R.id.bottomNavigationView);
+
+        bottomNavigationView.setSelectedItemId(R.id.nav_to_growSpace);
+
+        //Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.nav_to_growSpace:
+                        startActivity(new Intent(getApplicationContext(),activity_growspace.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.nav_to_login:
+                        startActivity(new Intent(getApplicationContext(),activity_logIn.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.nav_to_startseite:
+                        startActivity(new Intent(getApplicationContext(),activity_startseite.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 
     public void chanceActivity(View view) {
