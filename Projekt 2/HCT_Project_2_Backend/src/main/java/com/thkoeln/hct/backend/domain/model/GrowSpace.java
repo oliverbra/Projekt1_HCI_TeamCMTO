25cd501684290a71b57a6ea68661618f29f1296a
aobaidi410 <81808059+aobaidi410@users.noreply.github.com>
2021-09-20 15:29:11 +0200

    GrowSpaceService update um user and Review erweitert

--- Projekt 2/HCT_Project_2_Backend/src/main/java/com/thkoeln/hct/backend/domain/model/GrowSpace.java
+++ Projekt 2/HCT_Project_2_Backend/src/main/java/com/thkoeln/hct/backend/domain/model/GrowSpace.java
@@ -13,7 +13,7 @@ import java.util.Set;
 @Entity
 @Table(name = "grow_space")
 @Data
-@JsonIgnoreProperties("reviews")
+//@JsonIgnoreProperties("reviews")
 public class GrowSpace {
 
     public GrowSpace() {}
